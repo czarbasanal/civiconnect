bcaa08361e41251b31bfb400f0145fe95565a5f1
Mathew Alcoseba <mathewalcoseba25@gmail.com>
2024-06-07 13:51:49 +0800

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'screens/notification_screen.dart';
-import 'screens/profile_screen.dart';
-import 'screens/search_screen.dart';
-import 'screens/write_screen.dart';
+import 'screens/signup_screen.dart';
 import 'widgets/bottom_navigation_bar.dart';
-import 'package:civiconnect/screens/signup_screen.dart';
-// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,44 +13,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: '',
       theme: ThemeData(
+        fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
       ),
-      home: MainScreen(),
+      home: SignUpScreen(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _screens = <Widget>[
-    HomeScreen(),
-    SearchScreen(),
-    WriteScreen(),
-    NotificationScreen(),
-    ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavBar(
-        selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
-      ),
-      // home: SignUpScreen(),
-    );
+    return BottomNavBar();
   }
 }
--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -1,29 +1,48 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:civiconnect/screens/home_screen.dart';
+import 'package:civiconnect/screens/notification_screen.dart';
+import 'package:civiconnect/screens/profile_screen.dart';
+import 'package:civiconnect/screens/search_screen.dart';
+import 'package:civiconnect/screens/write_screen.dart';
 
-class BottomNavBar extends StatelessWidget {
-  final int selectedIndex;
-  final Function(int) onItemTapped;
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
 
-  const BottomNavBar({
-    Key? key,
-    required this.selectedIndex,
-    required this.onItemTapped,
-  }) : super(key: key);
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _screens = <Widget>[
+    HomeScreen(),
+    SearchScreen(),
+    WriteScreen(),
+    NotificationScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final navBarIcons = <BottomNavigationBarItem>[
       BottomNavigationBarItem(
         icon: Icon(
-          selectedIndex == 0 ? CupertinoIcons.house_fill : CupertinoIcons.house,
+          _selectedIndex == 0
+              ? CupertinoIcons.house_fill
+              : CupertinoIcons.house,
           size: 26,
         ),
         label: '',
       ),
       BottomNavigationBarItem(
         icon: Icon(
-          selectedIndex == 1
+          _selectedIndex == 1
               ? CupertinoIcons.search_circle_fill
               : CupertinoIcons.search_circle,
           size: 26,
@@ -32,21 +51,21 @@ class BottomNavBar extends StatelessWidget {
       ),
       BottomNavigationBarItem(
         icon: Icon(
-          selectedIndex == 2 ? Icons.create : Icons.create_outlined,
+          _selectedIndex == 2 ? Icons.create : Icons.create_outlined,
           size: 26,
         ),
         label: '',
       ),
       BottomNavigationBarItem(
         icon: Icon(
-          selectedIndex == 3 ? CupertinoIcons.bell_fill : CupertinoIcons.bell,
+          _selectedIndex == 3 ? CupertinoIcons.bell_fill : CupertinoIcons.bell,
           size: 26,
         ),
         label: '',
       ),
       BottomNavigationBarItem(
         icon: Icon(
-          selectedIndex == 4
+          _selectedIndex == 4
               ? CupertinoIcons.video_camera_solid
               : CupertinoIcons.video_camera,
           size: 26,
@@ -55,40 +74,42 @@ class BottomNavBar extends StatelessWidget {
       ),
     ];
 
-    return Stack(
-      children: [
-        SizedBox(
-          height: 75,
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            elevation: 0,
-            // backgroundColor: AppStyles.colorScheme.primary,
-            items: navBarIcons,
-            currentIndex: selectedIndex,
-            onTap: onItemTapped,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-          ),
-        ),
-        Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            height: 3,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  spreadRadius: 0.1,
-                  blurRadius: 5,
-                  offset: Offset(0, 2),
-                ),
-              ],
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: Stack(
+        children: [
+          SizedBox(
+            height: 75,
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              elevation: 0,
+              items: navBarIcons,
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
             ),
           ),
-        ),
-      ],
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 3,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 0.1,
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
